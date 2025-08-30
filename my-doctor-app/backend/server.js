deb44922d961be8f95ef2ee1ee6e17d5d8cf196e
dikshita <dikshitamehta9@gmail.com>
2025-08-30 22:57:11 +0530

    updated server file

--- my-doctor-app/backend/server.js
+++ my-doctor-app/backend/server.js
@@ -61,9 +61,7 @@ app.post('/api/appointments', (req, res) => {
     });
   });
 });
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'));
-});
+
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
 });
