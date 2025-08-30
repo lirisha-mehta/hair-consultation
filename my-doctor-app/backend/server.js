021d922ed73d69ca93199840ec35811667289aba
dikshita <dikshitamehta9@gmail.com>
2025-08-30 23:01:01 +0530

    updated server file

--- my-doctor-app/backend/server.js
+++ my-doctor-app/backend/server.js
@@ -5,9 +5,9 @@ const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static(path.join(__dirname, '../build')));
+// app.use(express.json());
+// app.use(cors());
+// app.use(express.static(path.join(__dirname, '../build')));
 
 const db = mysql.createConnection({
   host: 'localhost',
