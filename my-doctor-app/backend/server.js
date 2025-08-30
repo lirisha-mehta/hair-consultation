ec23a1a668aa695ad74dcb5ba882cd020d2633df
dikshita <dikshitamehta9@gmail.com>
2025-08-30 23:58:32 +0530

    updated key

--- my-doctor-app/backend/server.js
+++ my-doctor-app/backend/server.js
@@ -58,6 +58,6 @@ app.post('/api/appointments', (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(21820, () => {
+  console.log('Server is running on port 21820');
 });
